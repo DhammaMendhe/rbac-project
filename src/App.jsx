99330425605bd13e816a06be8e15ff610192893a
DhammaMendhe <send2dhammadipmendhe@gmail.com>
2024-11-30 09:14:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod, a iusto?
+        Rem, alias! Illo laudantium cum sed reprehenderit dolore distinctio
+        culpa, quos totam placeat architecto tempora asperiores. Minus,
+        reprehenderit quibusdam?
+      </p>
+    </>
+  );
+}
+
+export default App;
